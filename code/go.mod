cedefbad13f9f52726689857cf10c9733b5c2d85
Thiago Alves Pereira <thiago-alvesp@live.com>
2023-12-16 21:47:51 -0300

    tentativa de utilizar o gorilla/mux o router não funcionou

--- code/go.mod
+++ code/go.mod
@@ -2,4 +2,9 @@ module github.com/thiagoalvesp/ElbAsgLambdaEcs/tree/main/code
 
 go 1.20
 
-require github.com/aws/aws-lambda-go v1.41.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.41.0 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.0
+	github.com/bitly/go-simplejson v0.5.1
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- code/main.go
+++ code/main.go
@@ -1,50 +1,116 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/bitly/go-simplejson"
+	"github.com/gorilla/mux"
 )
 
 type MessageResponse struct {
 	Counter int64
 }
 
-func main() {
-	lambda.Start(handler)
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("loggingMiddleware")
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func handler(request events.ALBTargetGroupRequest) (
-	events.ALBTargetGroupResponse, error) {
+func CounterHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("Start Proccess")
+	json := simplejson.New()
+	json.Set("Counter", 30)
 
-	fmt.Println("Body")
-	fmt.Println(request.Body)
-
-	messageResponse := MessageResponse{Counter: 0}
-	jsonMessageResponse, err := json.Marshal(messageResponse)
-	jsonMessageResponseString := string(jsonMessageResponse)
-
+	payload, err := json.MarshalJSON()
 	if err != nil {
-		fmt.Println("Erro ao codificar para JSON:", err)
-		response := events.ALBTargetGroupResponse{
-			StatusCode: 500,
-		}
-		return response, err
+		log.Println(err)
 	}
 
-	headers := make(map[string]string)
-	headers["Content-Type"] = "application/json"
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
 
-	response := events.ALBTargetGroupResponse{
-		StatusCode:      200,
-		Body:            jsonMessageResponseString,
-		IsBase64Encoded: false,
-		Headers:         headers,
-	}
-
-	return response, nil
 }
+
+func AddCounterHandler(w http.ResponseWriter, r *http.Request) {
+
+	json := simplejson.New()
+	json.Set("Counter", 10)
+
+	payload, err := json.MarshalJSON()
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
+
+}
+
+func main() {
+
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Not found", r.RequestURI)
+		http.Error(w, fmt.Sprintf("Not found: %s", r.RequestURI), http.StatusNotFound)
+	})
+
+	r.HandleFunc("/add", AddCounterHandler)
+	r.HandleFunc("/", CounterHandler)
+
+	r.Use(loggingMiddleware)
+
+	if runtime_api, _ := os.LookupEnv("AWS_LAMBDA_RUNTIME_API"); runtime_api != "" {
+		log.Println("Starting up in Lambda Runtime")
+		adapter := gorillamux.NewALB(r)
+		lambda.Start(adapter.ProxyWithContext)
+	} else {
+		log.Println("Starting up on own")
+		srv := &http.Server{
+			Addr:    ":8080",
+			Handler: r,
+		}
+		_ = srv.ListenAndServe()
+	}
+
+}
+
+// func handler(request events.ALBTargetGroupRequest) (
+// 	events.ALBTargetGroupResponse, error) {
+
+// 	fmt.Println("Start Proccess")
+
+// 	fmt.Println("Body")
+// 	fmt.Println(request.Body)
+
+// 	messageResponse := MessageResponse{Counter: 0}
+// 	jsonMessageResponse, err := json.Marshal(messageResponse)
+// 	jsonMessageResponseString := string(jsonMessageResponse)
+
+// 	if err != nil {
+// 		fmt.Println("Erro ao codificar para JSON:", err)
+// 		response := events.ALBTargetGroupResponse{
+// 			StatusCode: 500,
+// 		}
+// 		return response, err
+// 	}
+
+// 	headers := make(map[string]string)
+// 	headers["Content-Type"] = "application/json"
+
+// 	response := events.ALBTargetGroupResponse{
+// 		StatusCode:      200,
+// 		Body:            jsonMessageResponseString,
+// 		IsBase64Encoded: false,
+// 		Headers:         headers,
+// 	}
+
+// 	return response, nil
+// }
